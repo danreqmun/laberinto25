79db1f0d063b1cdf6c214e3746899e2fad6c4a4b
danreqmun <165500053+danreqmun@users.noreply.github.com>
2025-06-01 18:03:32 +0200

    cambio menor flecha

--- flecha.py
+++ flecha.py
@@ -4,7 +4,7 @@ from decorator import Decorator
 class Flecha(Decorator):
     def __init__(self, elemento):
         super().__init__(elemento)
-        self.activa = False
+        self.activa = True
 
     def entrar(self, ente):
         # Primero se ejecuta la lógica original del elemento decorado
@@ -12,7 +12,7 @@ class Flecha(Decorator):
 
         # Ahora la lógica del cohete
         if self.activa:
-            print(f"{COLOR.ALGOMALO} {ente} le ha dado un cohete (decorator) {COLOR.FIN}")
+            print(f"{COLOR.ALGOMALO} {ente} le ha dado un flecha (decorator) {COLOR.FIN}")
             ente.vidas -= 1
             if ente.vidas <= 0:
                 ente.vidas = 0
@@ -20,7 +20,7 @@ class Flecha(Decorator):
             print(f"{COLOR.ORADOR} {ente} pierde una vida. Vidas restantes: {ente.vidas} {COLOR.FIN}")
             self.activa = False
         else:
-            print(f"{COLOR.ORADOR}El cohete del decorador ya se usó{COLOR.FIN}")
+            print(f"{COLOR.ORADOR}La flecha del decorador ya se usó{COLOR.FIN}")
 
 
     def esFlecha(self):
